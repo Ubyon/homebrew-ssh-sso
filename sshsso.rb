e1f718bfa920e886711b42b513d1aa3e80e7a17c
Chintan Vora <chintan.vora@ubyon.com>
2022-06-04 23:11:04 -0700

    "Version 0.0.1"

---
+++ sshsso.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sshsso < Formula
+  desc "Sshsso"
+  homepage "https://github.com/Ubyon/ssh-sso"
+  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "46d816e6224b8271f39cee4398776f2cf4b4fc2c6921fa28255cea5573725e02"
+  license "Ubyon Inc"
+  depends_on "openssl"
+  def install
+    bin.install 'proxytunnel'
+  end
+
+end
