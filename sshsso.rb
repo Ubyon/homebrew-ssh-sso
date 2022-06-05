e59d2d4affc85de2ba508f8d062a92dc458beeef
Chintan Vora <chintan.vora@ubyon.com>
2022-06-05 14:59:49 -0700

    "Version 0.0.2"

--- sshsso.rb
+++ sshsso.rb
@@ -3,7 +3,7 @@ class Sshsso < Formula
   homepage "https://github.com/Ubyon/ssh-sso"
   url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.0.2.tar.gz"
   version "0.0.2"
-  sha256 "9c413e1ba834fa56103f79a939892df7e530b25482d3d3cee6e03f73aeb7ff54"
+  sha256 "0b691189b358e8a583cdce4376aca67d71f43f878f7daef923d55200207cd717"
   license "Ubyon Inc"
   depends_on "openssl"
   def install
