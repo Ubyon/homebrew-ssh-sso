3572503701d0775f7978015e1387c000aaf29fe1
Chintan Vora <chintan.vora@ubyon.com>
2022-06-05 22:49:38 -0700

    "Version 0.0.5"

--- sshsso.rb
+++ sshsso.rb
@@ -11,4 +11,17 @@ class Sshsso < Formula
     libexec.install Dir['*']
     bin.write_jar_script libexec/'ssh-sso-helper.jar', 'ssh-sso-helper'
   end
+  def caveats; <<~EOS
+    ssh-sso-helper uses "ssh configuration" to support SSO using ssh.
+    1: Create configuration file mkdir ~/.ssh/config (If Not Present)
+    2: Create ubyon ssh configuration file mkdir ~/.ssh/usc/config
+    3: Update  ~/.ssh/config, echo 'Include usc/config' >> ~/.ssh/config
+    4: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
+    5: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/0.0.5/libexec/application.yaml
+    
+    Once everything is configured, typical use-case
+    1: ssh login (Helps you with SSO and updates ~/.ssh/usc/config accordingly)
+    2: ssh ubuntu@10.182.0.15 -p 9023
+  EOS
+  end
 end
