e8aa3431b619dda4d4cf69d85778f581bf387cfc
Chintan Vora <chintan.vora@ubyon.com>
2022-06-05 22:01:31 -0700

    "Version 0.0.3"

--- sshsso.rb
+++ sshsso.rb
@@ -6,7 +6,6 @@ class Sshsso < Formula
   sha256 "777abcb860a1186bfdd67b01f27f8f32fb3d9651188273a71fa3a5692defb926"
   license "Ubyon Inc"
   depends_on "openssl"
-  depends_on java: '1.8+'
   def install
     bin.install 'java-app-run'
     # libexec.install Dir['*']
