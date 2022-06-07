f1f59375fae6d533cd57a13bf2608f1f58955dec
Chintan Vora <chintan.vora@ubyon.com>
2022-06-07 15:14:20 -0700

    "Version 0.1.5"

--- sshsso.rb
+++ sshsso.rb
@@ -1,9 +1,9 @@
 class Sshsso < Formula
   desc "Sshsso"
   homepage "https://github.com/Ubyon/ssh-sso"
-  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.4.tar.gz"
-  version "0.1.4"
-  sha256 "39841a1553919d3307555058817860f6b5a768569366a1bbbf00f644c057a413"
+  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.5.tar.gz"
+  version "0.1.5"
+  sha256 "0208c3c7355ede0393baa6e1c41005f21e4105dd704d804493f2f04c07470261"
   license "Ubyon Inc"
   depends_on "openssl"
   def install
@@ -14,7 +14,8 @@ class Sshsso < Formula
 
   def caveats; <<~EOS
     ssh-sso-helper uses "ssh configuration" to support SSO using ssh.
-    1: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
+    1: export SSH_HOME=/Users/chintanvora/.ssh
+    2: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
     2: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml    
     3: ssh-sso-helper (To create init configuration) 
     4: ssh ubuntu@10.182.0.15
