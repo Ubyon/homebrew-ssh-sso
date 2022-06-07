31c422da56748510dad6125f557fca8f2326a482
Chintan Vora <chintan.vora@ubyon.com>
2022-06-06 20:22:05 -0700

    "Version 0.1.3"

--- sshsso.rb
+++ sshsso.rb
@@ -1,32 +1,23 @@
 class Sshsso < Formula
   desc "Sshsso"
   homepage "https://github.com/Ubyon/ssh-sso"
-  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "5531b01e2ee6f13258d4c0c9eeb6855fce32140a8c07070fc1d484942370dc8f"
+  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.3.tar.gz"
+  version "0.1.3"
+  sha256 "8249a6b127fe2fdf1f259039806c696e9b7aef1df9ee085c2b5db04298ef9748"
   license "Ubyon Inc"
   depends_on "openssl"
   def install
     bin.install 'proxytunnel'
     libexec.install Dir['*']
     bin.write_jar_script libexec/'ssh-sso-helper.jar', 'ssh-sso-helper'
-    system "mkdir", "-p", "/tmp/ubyon"
-    system "cp", libexec/'post_install.sh', "/tmp/ubyon"
-  end
-  def post_install
-    system "/bin/sh", "/tmp/ubyon/post_install.sh"
   end
 
   def caveats; <<~EOS
     ssh-sso-helper uses "ssh configuration" to support SSO using ssh.
-    1: Create configuration file mkdir ~/.ssh/config (If Not Present)
-    2: Create ubyon ssh configuration file mkdir ~/.ssh/usc/config
-    3: Update  ~/.ssh/config, echo 'Include usc/config' >> ~/.ssh/config
-    4: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
-    5: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml
-    
-    Once everything is configured, typical use-case
-    1: ssh ubuntu@10.182.0.15 -p 9023
+    1: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
+    2: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml    
+    3: ssh-sso-helper (To create init configuration) 
+    4: ssh ubuntu@10.182.0.15
   EOS
   end
 end
