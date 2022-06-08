1836acd62ee29cfdec68cbbabfd242722eddd67f
Chintan Vora <chintan.vora@ubyon.com>
2022-06-08 16:14:44 -0700

    "Version 0.1.8"

--- sshsso.rb
+++ sshsso.rb
@@ -1,9 +1,9 @@
 class Sshsso < Formula
   desc "Sshsso"
   homepage "https://github.com/Ubyon/ssh-sso"
-  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.7.tar.gz"
-  version "0.1.7"
-  sha256 "ef48f61f270427c23efaa6643e3c290d1fb38be574e752738a78e84cb01a2f38"
+  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.8.tar.gz"
+  version "0.1.8"
+  sha256 "b1bf4c783cd24cbdedfa1806ca93d9aec48707b08ffa31f31c8f3230bd150c0a"
   license "Ubyon Inc"
   depends_on "openssl"
   def install
@@ -14,11 +14,14 @@ class Sshsso < Formula
 
   def caveats; <<~EOS
     ssh-sso-helper uses "ssh configuration" to support SSO using ssh.
-    1: export SSH_HOME=/Users/chintanvora/.ssh
-    2: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml"
-    3: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml    
-    4: ssh-sso-helper (To create init configuration) 
-    5: ssh ubuntu@10.182.0.15
+    1: export SSH_CONFIG_DIR=<SSH Configuration Directory of the user>. Make sure if has config file present
+    2: export PROXY_URL=<Proxy URL>. Proxy/Edge url is the endpoint protecting resources using authentication/authorization.
+    3: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
+    4: export INIT_CONFIG=true for Initial Configuration 
+    5: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml     
+    6: ssh-sso-helper (To create init configuration) 
+    7: export INIT_CONFIG=false after initial configuration is done.
+    8: ssh ubuntu@server (Should trigger SSO and store identity) and help you get through.
   EOS
   end
 end
