e5089a5586597b162adebd64fdf1b0ee8a1f2f81
Chintan Vora <chintan.vora@ubyon.com>
2022-06-09 11:32:43 -0700

    "Version 0.1.9"

--- sshsso.rb
+++ sshsso.rb
@@ -16,12 +16,11 @@ class Sshsso < Formula
     ubyonlogin uses "ssh configuration" to support SSO using ssh.
     1: export SSH_CONFIG_DIR=<SSH Configuration Directory of the user>. Make sure if has config file present
     2: export PROXY_URL=<Proxy URL>. Proxy/Edge url is the endpoint protecting resources using authentication/authorization.
-    3: export UBYON_LOGIN=false for Initial Configuration 
-    4: ubyonlogin configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
-    5: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml     
-    6: ubyonlogin (To create init configuration) 
-    7: export UBYON_LOGIN=true for obtaining valid token.
-    8: ssh ubuntu@server (Should trigger SSO and store identity) and help you get through.
+    3: ubyonlogin configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
+    4: Run ubyonlogin (To create init configuration)
+    5: export UBYON_LOGIN=true for obtaining valid token.
+    6: Run ubyonlogin (for obtaining valid token) 
+    7: ssh ubuntu@server.
   EOS
   end
 end
