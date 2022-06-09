acb3e547253cb5eefe743663a9e88cacb5696c67
Chintan Vora <chintan.vora@ubyon.com>
2022-06-09 11:29:25 -0700

    "Version 0.1.9"

--- sshsso.rb
+++ sshsso.rb
@@ -1,26 +1,26 @@
 class Sshsso < Formula
   desc "Sshsso"
   homepage "https://github.com/Ubyon/ssh-sso"
-  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.8.tar.gz"
-  version "0.1.8"
-  sha256 "b1bf4c783cd24cbdedfa1806ca93d9aec48707b08ffa31f31c8f3230bd150c0a"
+  url "https://github.com/Ubyon/ssh-sso/archive/refs/tags/v0.1.9.tar.gz"
+  version "0.1.9"
+  sha256 "ebf2b47d615e4f6bb1ced42763f22b34a3c7ba4cd962c53ed3f5cf63a53f9e8a"
   license "Ubyon Inc"
   depends_on "openssl"
   def install
     bin.install 'proxytunnel'
     libexec.install Dir['*']
-    bin.write_jar_script libexec/'ssh-sso-helper.jar', 'ssh-sso-helper'
+    bin.write_jar_script libexec/'ubyonlogin.jar', 'ubyonlogin'
   end
 
   def caveats; <<~EOS
-    ssh-sso-helper uses "ssh configuration" to support SSO using ssh.
+    ubyonlogin uses "ssh configuration" to support SSO using ssh.
     1: export SSH_CONFIG_DIR=<SSH Configuration Directory of the user>. Make sure if has config file present
     2: export PROXY_URL=<Proxy URL>. Proxy/Edge url is the endpoint protecting resources using authentication/authorization.
-    3: export INIT_CONFIG=true for Initial Configuration 
-    4: ssh-sso-helper configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
+    3: export UBYON_LOGIN=false for Initial Configuration 
+    4: ubyonlogin configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
     5: export SPRING_CONFIG_LOCATION=/usr/local/Cellar/sshsso/<version>/libexec/application.yaml     
-    6: ssh-sso-helper (To create init configuration) 
-    7: export INIT_CONFIG=false after initial configuration is done.
+    6: ubyonlogin (To create init configuration) 
+    7: export UBYON_LOGIN=true for obtaining valid token.
     8: ssh ubuntu@server (Should trigger SSO and store identity) and help you get through.
   EOS
   end
