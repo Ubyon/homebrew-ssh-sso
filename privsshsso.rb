50cde23f046b38cfb9088d9cdf0918739956aa3b
Chintan Vora <chintan.vora@ubyon.com>
2022-06-14 13:36:06 -0700

    "Version 0.2.4"

--- privsshsso.rb
+++ privsshsso.rb
@@ -1,30 +1,5 @@
 require "formula"
 
-class Sshsso < Formula
-  desc "privsshsso"
-  homepage "https://github.com/Ubyon/ssh-sso"
-  url "https://github.com/Ubyon/ssh-sso/releases/download/v0.2.4/v0.2.4.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.2.4"
-  sha256 "cd871fedb61936cb74c4d0bcc32f6934b95d16dee2fa70b41d7f31dc9f0b3f36"
-  license "Ubyon Inc"
-  depends_on "openssl"
-  def install
-    bin.install 'proxytunnel'
-    libexec.install Dir['*']
-    bin.write_jar_script libexec/'ubyonlogin.jar', 'ubyonlogin'
-  end
-
-  def caveats; <<~EOS
-    ubyonlogin uses "ssh configuration" to support SSO using ssh.
-    1: export SSH_CONFIG_DIR=<SSH Configuration Directory of the user>. Make sure if has config file present
-    2: export PROXY_URL=<Proxy URL>. Proxy/Edge url is the endpoint protecting resources using authentication/authorization.
-    3: ubyonlogin configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
-    4: Run ubyonlogin (To obtain valid token)
-    5: ssh ubuntu@server.
-  EOS
-  end
-end
-
 # Save this file as `lib/private_strategy.rb`
 # Add `require_relative "lib/private_strategy"` to your formula.
 #
@@ -162,5 +137,32 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
   def fetch_release_metadata
     release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
     GitHub.open_api(release_url)
+  end
+end
+
+
+
+class Sshsso < Formula
+  desc "privsshsso"
+  homepage "https://github.com/Ubyon/ssh-sso"
+  url "https://github.com/Ubyon/ssh-sso/releases/download/v0.2.4/v0.2.4.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.2.4"
+  sha256 "cd871fedb61936cb74c4d0bcc32f6934b95d16dee2fa70b41d7f31dc9f0b3f36"
+  license "Ubyon Inc"
+  depends_on "openssl"
+  def install
+    bin.install 'proxytunnel'
+    libexec.install Dir['*']
+    bin.write_jar_script libexec/'ubyonlogin.jar', 'ubyonlogin'
+  end
+
+  def caveats; <<~EOS
+    ubyonlogin uses "ssh configuration" to support SSO using ssh.
+    1: export SSH_CONFIG_DIR=<SSH Configuration Directory of the user>. Make sure if has config file present
+    2: export PROXY_URL=<Proxy URL>. Proxy/Edge url is the endpoint protecting resources using authentication/authorization.
+    3: ubyonlogin configuration file is stored at "/usr/local/Cellar/sshsso/<version>/libexec/application.yaml". Make sure server domains are appropriately defined.
+    4: Run ubyonlogin (To obtain valid token)
+    5: ssh ubuntu@server.
+  EOS
   end
 end
